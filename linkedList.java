3e87351340c2f3e8f001fab848fcc8beccea853c
Brady Key <bradykey@gmail.com>
2014-10-08 16:12:47 -0600

    Added an abstract class to the file
    The LList generic abstract class was added to the file so that there
    would be some modularity involved.

--- linkedList.java
+++ linkedList.java
@@ -1,14 +1,28 @@
-public class LList<T> {
+public abstract class LList<T>{
+	/**
+	* Insert an element to the head of the 
+	* linked list.
+	*/
+	public bool insert(LList<T> list, T data){
+		LListElement<T> element = new LListElement<T>(data);
+		element.setNext(list);
+
+		// return the new head to the caller
+		return element;
+	}
+}
+
+public class LListElement<T> extends LList<T> {
 
 	// private global data members
 	private T data;
-	private LList<T> next;
+	private LListElement<T> next;
 
 	/**
 	* Default constructor for a 
-	* @link{LList<T>} element.
+	* @link{LListElement<T>} element.
 	*/
-	public LList(T value){
+	public LListElement(T value){
 		data = value;
 	}
 
@@ -23,11 +37,16 @@ public class LList<T> {
 		this.data = data;
 	}
 
-	public LList<T> getNext(){
+	public LListElement<T> getNext(){
 		return next;
 	}
 
-	public void setNext(LList<T> next){
+	public void setNext(LListElement<T> next){
 		this.next = next;
 	}
+}
+
+public static void main(String[] args) {
+	// create a linked list
+	LListElement<Int> l1 = new LListElement<Int>(10);	
 }
